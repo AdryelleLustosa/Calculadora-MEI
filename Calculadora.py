02a97ea1cf07964d78723eaac5d6534ac88726d3
AdryelleLustosa <124699259+AdryelleLustosa@users.noreply.github.com>
2025-04-08 21:14:24 -0300

    Inicialização Projeto

---
+++ Calculadora.py
@@ -0,0 +1,35 @@
+# Essa é um código para analisar de forma simples se o empreendedor precisa ou não fazer sua declaração de imposto de renda. 
+
+####### Funções ######
+
+class MEI_Calculadora:
+    
+    def __init__(self, faturamento, tipo, despesas):
+        self.faturamento = faturamento
+        self.despesas = tipo
+        self.tipo = despesas
+
+    def calcParcelaIsenta (self):
+        if self.tipo == 1: #Serviços
+            
+        
+
+       
+    
+    def parcelaTributavel (self, parteIsenta, parteTributavel):
+        
+        self.parteTributavel = self.faturamento - self.despesas - 
+
+        
+
+    def validarMEi(self, faturamento):
+        if self.faturamento > 81000:
+            print(f"Seu faturamento de {self.faturamento} é maior do que o estabelecido pela lei....")
+        
+
+        
+        
+
+        
+     
+
