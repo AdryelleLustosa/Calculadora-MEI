00b78f2059248668c56c85860b5fbb503d466819
AdryelleLustosa <124699259+AdryelleLustosa@users.noreply.github.com>
2025-04-13 18:43:59 -0300

    Implementação Inicial Visual

---
+++ app_streamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Título do dashboard
+st.title("Calculadora MEI – Preciso Declarar Imposto de Renda?")
+
+# Seção de métricas
+st.info("Descubra rapidamente se você, MEI, precisa declarar o Imposto de Renda em 2025.")
